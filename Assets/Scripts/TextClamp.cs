d75013d96c794cda25497a86a4060138ecac8c2a
Brandon <bvasquez1994@gmail.com>
2021-04-27 14:40:36 -0400

    Uploading new stable Version 1.12 and its Zip file.

---
+++ Assets/Scripts/TextClamp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextClamp : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI clampText;
+
+    void Update()
+    {
+        Vector3 gameObjectPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        clampText.transform.position = gameObjectPos;
+    }
+}
